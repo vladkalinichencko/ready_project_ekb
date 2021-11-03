25954f641ad0fddce05e0c11d3100211f09b47ec
Ilyin Alexander <aleksanilin@ozon.ru>
2021-11-03 12:16:58 +0500

    add make_done method

--- source/urls.py
+++ source/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_tasks
+from todo.views import get_tasks, make_done
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('get_tasks/', get_tasks)
+    path('get_tasks/', get_tasks),
+    path('make_done/<int:task_id>/', make_done)
 ]
--- todo/views.py
+++ todo/views.py
@@ -8,9 +8,21 @@ def get_tasks(request):
     for todo in todo_list:
         data['data'].append(
             {
+                'id': todo.id,
                 'number': todo.number,
                 'text': todo.text,
                 'is_done': todo.is_done
             }
         )
     return JsonResponse(data=data)
+
+
+def make_done(request, task_id):
+    try:
+        task = Todo.objects.get(id=task_id)
+    except Todo.DoesNotExist:
+        return JsonResponse(data={'result': False})
+    task.is_done = True
+    task.save()
+
+    return JsonResponse(data={'result': True})
